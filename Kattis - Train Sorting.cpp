c260277d7ccbf4a29a9eb980582f27a854be8ad0
Shahain Manujith <86903975+itsShahain@users.noreply.github.com>
2024-06-21 22:22:30 +0530

    Create Kattis - Train Sorting.cpp

---
+++ Kattis - Train Sorting.cpp
@@ -0,0 +1,60 @@
+#include <bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+
+using namespace std;
+using namespace __gnu_pbds;
+
+// #define FILE_IO
+
+typedef long long ll;
+typedef tree<string, null_type, less<string>, rb_tree_tag, tree_order_statistics_node_update> ost;
+const int dx[4]{1, 0, -1, 0}, dy[4]{0, 1, 0, -1}; // for every manhattan grid!
+
+void setIO(string s) {
+    freopen((s + "_in.txt").c_str(), "r", stdin);
+    freopen((s + "_out.txt").c_str(), "w", stdout);
+}
+
+void tc() {
+}
+
+void solve() {
+    int n;
+    scanf("%d", &n);
+    deque<int> dq;
+    int x;
+    for (int i = 0; i < n; i++) {
+        scanf("%d", &x);
+        dq.push_front(x); dq.push_back(x);
+    }
+    // Finding LIS or LDS gives the same length
+    vector<int> L(n + 5);
+    int k = 0;
+    for (int i = 0; i < dq.size(); i++) {
+        int low = 0, high = k, mid;
+        while (low < high) {
+            mid = low + (high - low) / 2;
+            if (L[mid] > dq[i]) {
+                high = mid;
+            } else {
+                low = mid + 1;
+            }
+        }
+        L[low] = dq[i];
+        if (low == k) k++;
+    }
+    printf("%d\n", max(0, k - 1));
+}
+
+// WOW: printf("%d%c", miss[i], " \n"[i==n-1]);
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+#ifdef FILE_IO
+    setIO("test");
+#endif
+    solve();
+    cout << flush;
+}
