e47af5d67e385c386cba751317fe0db175685668
Shahain Manujith <86903975+itsShahain@users.noreply.github.com>
2024-07-21 09:19:47 +0530

    Create Kattis - Shopaholic.cpp

---
+++ Kattis - Shopaholic.cpp
@@ -0,0 +1,44 @@
+#include <bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+
+using namespace std;
+using namespace __gnu_pbds;
+
+// #define FILE_IO
+
+typedef long long ll;
+typedef tree<string, null_type, less<string>, rb_tree_tag, tree_order_statistics_node_update> ost;
+const int dx[4]{1, 0, -1, 0}, dy[4]{0, 1, 0, -1}; // for every manhattan grid!
+
+void setIO(string s) {
+    freopen((s + "_in.txt").c_str(), "r", stdin);
+    freopen((s + "_out.txt").c_str(), "w", stdout);
+}
+
+void tc() {
+}
+
+void solve() {
+    int n; scanf("%d", &n);
+    vector<ll> xs(n);
+    for (int i = 0; i < n; i++) scanf("%lld", &xs[i]);
+    sort(xs.rbegin(), xs.rend());
+    int i = 0; ll ans = 0LL;
+    while (i <= xs.size() - 3) {
+        ans += xs[i + 2]; i += 3;
+    }
+    printf("%lld\n", ans);
+}
+
+// WOW: printf("%d%c", miss[i], " \n"[i==n-1]);
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+#ifdef FILE_IO
+    setIO("test");
+#endif
+    solve();
+    cout << flush;
+}
